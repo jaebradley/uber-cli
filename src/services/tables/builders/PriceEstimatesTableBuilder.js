b23a7afe109df93be10a7350edc502a77062cc5f
jaebradley <jae.b.bradley@gmail.com>
2016-12-23 12:24:09 -0500

    refactoring some of the price estimates table builder

--- src/services/tables/builders/PriceEstimatesTableBuilder.js
+++ src/services/tables/builders/PriceEstimatesTableBuilder.js
@@ -8,71 +8,68 @@ import Utilities from '../../../Utilities';
 
 export default class PriceEstimatesTableBuilder {
   static build(estimates) {
-    let table = new Table();
-    table.push(
-      [
-        {
-          content: emoji.get('oncoming_automobile'),
-          hAlign: 'center'
-        },
-        {
-          content: emoji.get('money_with_wings'),
-          hAlign: 'center'
-        },
-        {
-          content: emoji.get('arrows_clockwise'),
-          hAlign: 'center'
-        },
-        {
-          content: emoji.get('hourglass_flowing_sand'),
-          hAlign: 'center'
-        },
-        {
-          content: `${emoji.get('boom')} Surge${emoji.get('boom')}`,
-          hAlign: 'center'
-        }
-      ]
-    );
-    estimates.estimates.forEach(function(estimate) {
-      if (estimate.productName !== 'TAXI') {
-        let surge = estimate.surgeMultiplier == 1
-          ? emoji.get('no_entry_sign')
-          : `${estimate.surgeMultiplier}x ${emoji.get('grimacing')}`;
-        table.push(
-          [
-            estimate.productName,
-            estimate.getFormattedRange(),
-            estimate.getFormattedDistance(),
-            Utilities.generateFormattedTime(estimate.duration),
-            surge
-          ]
-        );
-      }
-    });
-    table.push(
-      [
-        {
-          colSpan: 1,
-          content: emoji.get('round_pushpin'),
-          hAlign: 'center'
-        },
-        {
-          colSpan: 4,
-          content: estimates.start.name
-        }
-      ],
-      [
-        {
-          colSpan: 1,
-          content: emoji.get('end'),
-          hAlign: 'center'
-        },
-        {
-          colSpan: 4,
-          content: estimates.end.name
-        }
-      ]
-    );
+    let table = PriceEstimatesTableBuilder.buildInitialTable();
+    estimates.estimates.forEach(estimate => table.push(PriceEstimatesTableBuilder.buildEstimateRow(estimate)));
+    table.push(PriceEstimatesTableBuilder.buildLocationRow(estimates.start.name, false));
+    table.push(PriceEstimatesTableBuilder.buildLocationRow(estimates.end.name, true));
     return table.toString();
+  }
+
+  static getTableHeaders() {
+    return List.of(
+      emoji.get('oncoming_automobile'),
+      emoji.get('money_with_wings'),
+      emoji.get('arrows_clockwise'),
+      emoji.get('hourglass_flowing_sand'),
+      `${emoji.get('boom')} Surge${emoji.get('boom')}`
+    );
+  }
+
+  static buildInitialTable() {
+    let table = new Table();
+    PriceEstimatesTableBuilder.getTableHeaders()
+                              .forEach(header => table.push({
+                                content: header,
+                                hAlign: 'center'
+                              }));
+    return table;
+  }
+
+  static buildEstimateRow(estimate) {
+    if (estimate.productName !== 'TAXI') {
+      return [
+        estimate.productName,
+        estimate.getFormattedRange(),
+        estimate.getFormattedDistance(),
+        Utilities.generateFormattedTime(estimate.duration),
+        PriceEstimatesTableBuilder.buildSurgeMultiplierSymbol(estimate.surgeMultiplier)
+      ];
+    }
+  }
+
+  static buildSurgeMultiplierSymbol(surgeMultiplier) {
+    if (surgeMultiplier === 1) {
+      return emoji.get('no_entry_sign');
+    }
+
+    return `${estimate.surgeMultiplier}x ${emoji.get('grimacing')}`;
+  }
+
+  static buildLocationRow(name, isEnd) {
+    let symbol = emoji.get('round_pushpin');
+    if (isEnd) {
+      symbol = emoji.get('end');
+    }
+    return [
+      {
+        colSpan: 1,
+        content: symbol,
+        hAlign: 'center'
+      },
+      {
+        colSpan: 4,
+        content: name
+      }
+    ]
   }
 }
