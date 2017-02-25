da870ea1e4199898af50b41876f6e0be4cac3da8
jaebradley <jae.b.bradley@gmail.com>
2017-02-25 16:09:31 -0500

    testing

--- src/data/Unit.js
+++ src/data/Unit.js
@@ -2,7 +2,16 @@
 
 import {Enum} from 'enumify';
 
-export default class Unit extends Enum {}
+export default class Unit extends Enum {
+  static getDistanceUnit(name) {
+    const unit = Unit.enumValueOf(name.toUpperCase())
+    if (unit === Unit.SECOND) {
+      throw new TypeError('Not a distance unit');
+    }
+    return unit;
+  }
+}
+
 Unit.initEnum({
   MILE: {
     abbreviation: 'MI'
