64ed974a399aec50734ad81ed388fc4e4a2a1531
jaebradley <jae.b.bradley@gmail.com>
2017-02-25 23:39:45 -0500

    implement distance unit in command line execution

--- src/executables/uber-price.js
+++ src/executables/uber-price.js
@@ -9,10 +9,11 @@ let service = new CommandExecutionService();
 program
   .option('-s, --start <start>', 'specify start address')
   .option('-e, --end <end>', 'specify end address')
+  .option('-u, --unit <unit>', 'specify distance unit')
   .parse(process.argv);
 
 try {
-  service.executePriceEstimates(program.start, program.end)
+  service.executePriceEstimates(program.start, program.end, program.unit)
          .then(table => console.log(table));
 } catch (Error) {
   console.error('Could not get price estimates');
--- src/services/CommandExecutionService.js
+++ src/services/CommandExecutionService.js
@@ -2,6 +2,7 @@
 
 import program from 'commander';
 
+import DistanceUnit from '../data/DistanceUnit';
 import UberService from './UberService';
 import PriceEstimatesTableBuilder from './tables/builders/PriceEstimatesTableBuilder';
 import TimeEstimatesTableBuilder from './tables/builders/TimeEstimatesTableBuilder';
@@ -11,7 +12,7 @@ export default class CommandExecutionService {
     this.uberService = new UberService();
   }
 
-  executePriceEstimates(start, end) {
+  executePriceEstimates(start, end, distanceUnitName) {
     if (typeof start !== 'string') {
       throw new TypeError('start address should be a string');
     }
@@ -20,7 +21,12 @@ export default class CommandExecutionService {
       throw new TypeError('end address should be a string');
     }
 
-    return this.uberService.getPriceEstimates(start, end)
+    let distanceUnit = DistanceUnit.MILE;
+    if (typeof distanceUnitName === 'string') {
+      distanceUnit = DistanceUnit.enumValueOf(distanceUnitName.toUpperCase());
+    }
+
+    return this.uberService.getPriceEstimates(start, end, distanceUnit)
                            .then(estimates => PriceEstimatesTableBuilder.build(estimates));
   }
 
