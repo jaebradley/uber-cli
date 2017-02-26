489ec07c8d3c2014d79830346226b82b39e947bf
jaebradley <jae.b.bradley@gmail.com>
2017-02-25 23:10:00 -0500

    add price estimate formatter

---
+++ src/services/tables/builders/PriceEstimateFormatter.js
@@ -0,0 +1,35 @@
+'use es6';
+
+import CurrencySymbol from 'currency-symbol-map';
+
+import DistanceUnit from '../../../data/DistanceUnit';
+import DistanceConverter from '../../DistanceConverter';
+
+export default class PriceEstimateFormatter {
+  static getFormattedRange(range) {
+    const currencySymbol = CurrencySymbol(range.currencyCode);
+    return `${currencySymbol}${range.low}-${currencySymbol}${range.high}`;
+  }
+
+  static getFormattedDistance(distance) {
+    // 2 decimal places
+    const roundedDistanceValue = Math.round(distance.value * 100) / 100;
+    return `${roundedDistanceValue} ${PriceEstimateFormatter.getDistanceUnitAbbreviation(distance.unit)}.`;
+  }
+
+  static getDistanceUnitAbbreviation(unit) {
+    switch (unit) {
+      case DistanceUnit.MILE: {
+        return 'mi';
+      }
+
+      case DistanceUnit.KILOMETER: {
+        return 'km';
+      }
+
+      default: {
+        throw new TypeError('Unexpected distance unit');
+      }
+    }
+  }
+}
