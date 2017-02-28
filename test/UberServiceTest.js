bf6ed0affee5067f0409bd6703f37c1be445ed4d
jaebradley <jae.b.bradley@gmail.com>
2017-02-28 00:56:56 -0500

    update service test

--- test/UberServiceTest.js
+++ test/UberServiceTest.js
@@ -6,6 +6,7 @@ import chaiImmutable from 'chai-immutable';
 
 import {List} from 'immutable';
 
+import PriceEstimateQuery from '../src/data/PriceEstimateQuery';
 import UberService from '../src/services/UberService';
 import TimeEstimate from '../src/data/TimeEstimate';
 import DistanceUnit from '../src/data/DistanceUnit';
@@ -18,6 +19,11 @@ describe('Test Uber Service', function() {
   let service = new UberService();
   let address = '25 first street cambridge ma';
   let address2 = '114 line street somerville ma';
+  let priceEstimateQuery = new PriceEstimateQuery({
+    startAddress: address,
+    endAddress: address2,
+    distanceUnit: DistanceUnit.MILE
+  });
 
   it('tests time estimates fulfillment', function() {
     return service.getTimeEstimates(address).should.be.fulfilled;
@@ -29,11 +35,11 @@ describe('Test Uber Service', function() {
   });
 
   it('tests price estimates fulfillment', function() {
-    return service.getPriceEstimates(address, address2, DistanceUnit.MILE).should.be.fulfilled;
+    return service.getPriceEstimates(priceEstimateQuery).should.be.fulfilled;
   })
 
   it('tests price estimates fetching', function() {
-    return service.getPriceEstimates(address, address2, DistanceUnit.MILE)
+    return service.getPriceEstimates(priceEstimateQuery)
                   .then(results => console.log(results));
   });
 });
