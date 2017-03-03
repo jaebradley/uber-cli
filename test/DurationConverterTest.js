f27de4b0365085d22c9352234f1e2845a43d4e55
jaebradley <jae.b.bradley@gmail.com>
2017-03-03 00:06:41 -0500

    add duration converter and tests

---
+++ test/DurationConverterTest.js
@@ -0,0 +1,36 @@
+'use es6';
+
+import chai from 'chai';
+import chaiImmutable from 'chai-immutable';
+
+import Duration from '../src/data/Duration';
+import TimeUnit from '../src/data/TimeUnit';
+
+import DurationConverter from '../src/services/DurationConverter';
+
+chai.use(chaiImmutable);
+
+let expect = chai.expect;
+
+describe('Duration converter test', function() {
+  describe('Unit identifier test', function() {
+    expect(DurationConverter.getUnitConversionIdentifier(TimeUnit.SECOND)).to.equal('s');
+    expect(DurationConverter.getUnitConversionIdentifier(TimeUnit.MINUTE)).to.equal('min');
+  });
+
+  const length = 1.234;
+  const minuteDuration = 74.04;
+  const durationInSeconds = new Duration({
+    length: length,
+    unit: TimeUnit.SECOND
+  });
+  const durationInMinutes = new Distance({
+    length: minuteDuration,
+    unit: TimeUnit.MINUTE
+  });
+
+  describe('Duration conversion test', function() {
+    expect(DurationConverter.convert(durationInSeconds, TimeUnit.SECOND)).to.eql(durationInSeconds);
+    expect(DurationConverter.convert(durationInSeconds, TimeUnit.MINUTE)).to.eql(durationInMinutes);
+  });
+});
