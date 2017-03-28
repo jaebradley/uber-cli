127b495d64b9461dadb77c0f68717d0f7bcc7c44
Felix Sargent <felix.sargent@gmail.com>
2017-03-28 13:15:37 -0700

    Reformat uber-time to use Command better

--- src/executables/uber-time.js
+++ src/executables/uber-time.js
@@ -6,18 +6,15 @@ import CommandExecutionService from '../services/CommandExecutionService';
 
 let service = new CommandExecutionService();
 
-program
-  .arguments('<address>')
-  .action(address => {
-    try {
-        service.executeTimeEstimates(address)
-               .then(table => console.log(table));
-    } catch (Error) {
-      console.error('Could not get time estimates:\n', Error.message);
-    }
-  })
-  .parse(process.argv);
+program.parse(process.argv);
 
-if (typeof address !== 'string') {
-  console.error('Could not get time estimates:\n', "Address is required");
+var address = program.args.toString();
+try {
+    if (!address.length) {
+        throw new TypeError('Address is required');
+    }
+    service.executeTimeEstimates(address)
+        .then(table => console.log(table));
+} catch (Error) {
+    console.error('Could not get time estimates:\n', Error.message);
 }
