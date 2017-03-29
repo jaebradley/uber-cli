7c1b6d23f7b9ff728dec456f59f015bd5bf5b6bc
jaebradley <jae.b.bradley@gmail.com>
2017-03-29 00:15:52 -0400

    add test

---
+++ test/services/translators/estimates/TripPriceEstimatesTranslatorTest.js
@@ -0,0 +1,80 @@
+'use es6';
+
+import chai from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+chai.use(sinonChai);
+
+const expect = chai.expect;
+
+import { List } from 'immutable';
+
+import TripPriceEstimateTranslator from '../../../../src/services/translators/estimates/TripPriceEstimateTranslator';
+import TripPriceEstimatesTranslator from '../../../../src/services/translators/estimates/TripPriceEstimatesTranslator';
+
+describe('Trip Price Estimates Translation', function() {
+  const estimateTranslator = new TripPriceEstimateTranslator();
+  const translator = new TripPriceEstimatesTranslator(estimateTranslator);
+  const valid = { prices: [] };
+
+  describe('JSON Validation', () => {
+
+    describe('Valid', () => {
+      it('should be valid', () => {
+        expect(translator.isValid(valid)).to.be.true;
+      });
+    });
+
+    describe('Invalid', () => {
+      let invalid = {};
+
+      afterEach(function() {
+        expect(translator.isValid(invalid)).to.be.false;
+      });
+
+      it('invalid due to missing prices name field', () => {});
+
+      it('invalid due to wrong prices type', () => {
+        invalid[TripPriceEstimatesTranslator.getPricesFieldName()] = 1;
+      });
+    });
+  });
+
+  describe('Translate Estimates', () => {
+    const estimates = {
+      prices: [ 1, 2, 3 ]
+    };
+
+    describe('Invalid', () => {
+      before( () => {
+        this.isValid = sinon.stub(translator, 'isValid').returns(false);
+      });
+
+      after( () => {
+        this.isValid.restore();
+      });
+
+      it('throws for invalid estimate', () => {
+        expect(() => translator.translate({})).to.throw(Error);
+      });
+    });
+
+    describe('Valid', () => {
+      before( () => {
+        this.isValid = sinon.stub(translator, 'isValid').returns(true);
+        this.translateEstimate = sinon.stub(estimateTranslator, 'translate')
+                                      .callsFake(() => 'bar');
+      });
+
+      after( () => {
+        this.isValid.restore();
+        this.translateEstimate.restore();
+      });
+
+      it('translates valid estimates', () => {
+        const expected = List.of('bar', 'bar', 'bar');
+        expect(translator.translate(estimates)).to.eql(expected);
+      });
+    });
+  });
+});
