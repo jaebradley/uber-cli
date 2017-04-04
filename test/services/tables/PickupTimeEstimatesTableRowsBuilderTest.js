7432d6539b839540fb641e33dc909cb72c0e7c94
jaebradley <jae.b.bradley@gmail.com>
2017-04-03 21:55:27 -0400

    add test

--- test/services/tables/PickupTimeEstimatesTableRowsBuilderTest.js
+++ test/services/tables/PickupTimeEstimatesTableRowsBuilderTest.js
@@ -30,31 +30,49 @@ describe('Pickup Time Estimates Table Rows Builder', () => {
   };
   const estimates = List.of(estimate, anotherEstimate);
 
-  describe('builds rows', () => {
+  describe('groups by row', () => {
     it('empty estimates', () => {
-      expect(rowsBuilder.build(List())).to.eql(List());
+      expect(rowsBuilder.groupByTime(List())).to.eql(Map());
     });
 
     it('one estimate', () => {
       const durationFormatting = sinon.stub(durationFormatter, 'format').returns('foo');
-      const expectedJS = [
-        [
-          'foo',
-          ['jaebaebae']
-        ]
-      ];
-      expect(rowsBuilder.build(List.of(estimate)).toJS()).to.eql(expectedJS);
+      const expectedJS = {
+        'foo': ['jaebaebae']
+      };
+
+      expect(rowsBuilder.groupByTime(List.of(estimate)).toJS()).to.eql(expectedJS);
+
+      durationFormatting.restore();
     });
 
     it('duplicate estimates', () => {
-      const expectedJS = [
-        [
-          'foo',
-          ['jaebaebae', 'bae jadley']
-        ]
-      ];
+      const durationFormatting = sinon.stub(durationFormatter, 'format').returns('foo');
+      const expectedJS = {
+        'foo': ['jaebaebae', 'bae jadley']
+      };
 
-      expect(rowsBuilder.build(estimates).toJS()).to.eql(expectedJS)
+      expect(rowsBuilder.groupByTime(estimates).toJS()).to.eql(expectedJS)
+
+      durationFormatting.restore();
+    });
+  });
+
+  describe('builds', () => {
+    it('succeeds', () => {
+      const groups = Map({
+        'foo': ['bar', 'baz'],
+        'jae': ['bae']
+      });
+      const rowGrouping = sinon.stub(rowsBuilder, 'groupByTime').returns(groups);
+      const expected = List.of(
+        List.of('foo', 'bar, baz'),
+        List.of('jae', 'bae')
+      );
+
+      expect(rowsBuilder.build(estimates)).to.eql(expected);
+
+      rowGrouping.restore();
     });
   });
 });
