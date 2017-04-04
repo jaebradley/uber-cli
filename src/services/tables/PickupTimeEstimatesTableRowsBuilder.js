0607bbdc16e2b8f88d60e84d73bf83cfd40ae904
jaebradley <jae.b.bradley@gmail.com>
2017-04-03 21:42:34 -0400

    fix rows builder

--- src/services/tables/PickupTimeEstimatesTableRowsBuilder.js
+++ src/services/tables/PickupTimeEstimatesTableRowsBuilder.js
@@ -6,13 +6,20 @@ import emoji from 'node-emoji';
 
 export default class PickupTimeEstimatesTableRowsBuilder {
   constructor(durationFormatter) {
-    this.durationFormatter= durationFormatter;
+    this.durationFormatter = durationFormatter;
   }
 
   build(estimates) {
+    const estimatesGroupedByTime = this.groupByTime(estimates);
+    return List(estimatesGroupedByTime.entrySeq().map((entry) => {
+      return List.of(entry[0], entry[1].join(', '));
+    }));
+  }
+
+  groupByTime(estimates){
     let rows = Map();
     estimates.forEach((estimate) => {
-      const formattedDuration = this.durationFormatter.format(estimate.duration);
+      const formattedDuration = this.durationFormatter.format(estimate.estimatedDuration);
 
       if (rows.has(formattedDuration)) {
         let productNames = rows.get(formattedDuration);
@@ -22,6 +29,6 @@ export default class PickupTimeEstimatesTableRowsBuilder {
         rows = rows.set(formattedDuration, [estimate.productName]);
       }
     });
-    return rows.entrySeq();
+    return rows;
   }
 }
--- src/services/tables/builders/PickupTimeEstimatesTableBuilder.js
+++ src/services/tables/builders/PickupTimeEstimatesTableBuilder.js
@@ -11,7 +11,7 @@ export default class PickupTimeEstimatesTableBuilder {
 
   build(estimates) {
     let table = this.buildInitialTable(estimates.location.name);
-    this.rowsBuilder.build(estimates).forEach(row => table.push(row));
+    this.rowsBuilder.build(estimates.estimates).forEach(row => table.push(row.toJS()));
     return table.toString();
   }
 
