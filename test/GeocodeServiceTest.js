c1a6b9b1a1b6bf670f7551f9e4c8d54f4a8709dd
Jae Bradley <jae.b.bradley@gmail.com>
2018-06-04 08:33:34 -0400

    refactor(location): remove Location class

--- test/GeocodeServiceTest.js
+++ test/GeocodeServiceTest.js
@@ -5,7 +5,6 @@ import chaiImmutable from 'chai-immutable';
 import { List } from 'immutable';
 
 import GeocodeService from '../src/services/GeocodeService';
-import Location from '../src/data/Location';
 
 import geocodeFile from './files/geocode.json';
 
@@ -16,13 +15,13 @@ chai.should();
 describe('Test Geocode Service', () => {
   const service = new GeocodeService();
   const address = '25 first street cambridge ma';
-  const location = new Location({
+  const location = {
     name: '25 First St, Cambridge, MA 02141, USA',
     coordinate: {
       latitude: 42.369695,
       longitude: -71.07800569999999,
     },
-  });
+  };
   const locations = List.of(location);
 
   it('tests data fetching', () => service.getData(address).should.eventually.eql(geocodeFile));
