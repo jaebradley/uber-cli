4be45cbef7d3e2c5fbb1d1887e0fd17656e484cd
Jae Bradley <jae.b.bradley@gmail.com>
2018-06-04 08:19:18 -0400

    Update development environment (#55)
    * build(babel): update Babel configuration
    
    * build(config): update config files
    
    * style(linting): temporarily turn off linting

--- test/services/symbols/BaseSymbolService.test.js
+++ test/services/symbols/BaseSymbolService.test.js
@@ -1,50 +1,45 @@
-import chai from 'chai';
-
 import BaseSymbolService from '../../../src/services/symbols/BaseSymbolService';
-
-const expect = chai.expect;
-
 
 describe('BaseSymbolService Test', () => {
   const service = new BaseSymbolService();
 
   it('should throw when getting vehicle symbol', () => {
-    expect(() => service.getVehicleSymbol()).to.throw(Error);
+    expect(() => service.getVehicleSymbol()).toThrow();
   });
 
   it('should throw when getting price symbol', () => {
-    expect(() => service.getPriceSymbol()).to.throw(Error);
+    expect(() => service.getPriceSymbol()).toThrow();
   });
 
   it('should throw when getting trip distance symbol', () => {
-    expect(() => service.getTripDistanceSymbol()).to.throw(Error);
+    expect(() => service.getTripDistanceSymbol()).toThrow();
   });
 
   it('should throw when getting duration symbol', () => {
-    expect(() => service.getDurationSymbol()).to.throw(Error);
+    expect(() => service.getDurationSymbol()).toThrow();
   });
 
   it('should throw when getting surge symbol', () => {
-    expect(() => service.getSurgeSymbol()).to.throw(Error);
+    expect(() => service.getSurgeSymbol()).toThrow();
   });
 
   it('should throw when getting not applicable symbol', () => {
-    expect(() => service.getNotApplicableSymbol()).to.throw(Error);
+    expect(() => service.getNotApplicableSymbol()).toThrow();
   });
 
   it('should throw when getting surge present symbol', () => {
-    expect(() => service.getSurgePresentSymbol()).to.throw(Error);
+    expect(() => service.getSurgePresentSymbol()).toThrow();
   });
 
   it('should throw when getting destination symbol', () => {
-    expect(() => service.getDestinationSymbol()).to.throw(Error);
+    expect(() => service.getDestinationSymbol()).toThrow();
   });
 
   it('should throw when getting origin symbol', () => {
-    expect(() => service.getOriginSymbol()).to.throw(Error);
+    expect(() => service.getOriginSymbol()).toThrow();
   });
 
   it('should throw when getting maximum distance symbol', () => {
-    expect(() => service.getMaximumDistanceSymbol()).to.throw(Error);
+    expect(() => service.getMaximumDistanceSymbol()).toThrow();
   });
 });
